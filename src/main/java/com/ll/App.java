230fb79849c1be522f591a8c4299c4acc320885f
seng-han <kumjjick@icloud.com>
2024-11-24 15:45:31 +0900

    class refactor

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,56 @@
+package com.ll;
+import java.util.Scanner;
+
+
+public class App {
+    private Scanner scanner;
+    private int lastId;
+    private WiseSaying[] wiseSayings;
+    private int wiseSayingsSize;
+    public App() {
+        scanner = new Scanner(System.in);
+        lastId = 0;
+        wiseSayings = new WiseSaying[100];
+        wiseSayingsSize = 0;
+    }
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        addWiseSaying("나의 죽음을 적들에게 알리지 말라.", "이순신 장군");
+        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                actionAdd();
+            } else if (cmd.equals("목록")) {
+                actionList();
+            }
+        }
+        scanner.close();
+    }
+    private WiseSaying addWiseSaying(String content, String author) {
+        int id = ++lastId;
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings[wiseSayingsSize] = wiseSaying;
+        wiseSayingsSize++;
+        return wiseSaying;
+    }
+    private void actionAdd() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+        WiseSaying wiseSaying = addWiseSaying(content, author);
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.id));
+    }
+    private void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying == null) break;
+            System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+        }
+    }
+}
