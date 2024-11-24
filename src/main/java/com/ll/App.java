bad885129cfc2985cea426afd76dbe8e038ddb79
seng-han <kumjjick@icloud.com>
2024-11-24 16:15:29 +0900

    Apply lambda

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -16,8 +16,8 @@ public class App {
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
-        addWiseSaying("나의 죽음을 적들에게 알리지 말라.", "이순신 장군");
-        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+        //makeSampleData();
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -27,18 +27,28 @@ public class App {
                 actionAdd();
             } else if (cmd.equals("목록")) {
                 actionList();
+            } else if ( cmd.equals("삭제?id=1")){
+                actionDelete(1);
             }
         }
         scanner.close();
     }
+
+    private void makeSampleData() {
+        addWiseSaying("나의 죽음을 적들에게 알리지 말라.", "이순신 장군");
+        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+    }
+
     private WiseSaying addWiseSaying(String content, String author) {
         int id = ++lastId;
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
         wiseSayings.add(wiseSaying);
-        System.out.println("wiseSayings =" + wiseSayings);
 
         return wiseSaying;
     }
+
+    //액션 함수들
+
     private void actionAdd() {
         System.out.print("명언 : ");
         String content = scanner.nextLine();
@@ -50,8 +60,15 @@ public class App {
     private void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
-        for (WiseSaying wiseSaying : wiseSayings) {
-            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getAuthor()));
+
+       for(WiseSaying wiseSaying : wiseSayings.reversed()) {
+           System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
         }
+    }
+    private void actionDelete(int id) {
+        boolean removed = wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id); //람다함수
+
+        if (removed) System.out.println("%d 학번 명언을 삭제했습니다.".formatted(id));
+
     }
 }
