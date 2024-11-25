f3040b1c5919f43a9568c17005b81a8625c1fdce
seng-han <kumjjick@icloud.com>
2024-11-25 23:03:42 +0900

    scanner

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,29 +1,22 @@
 package com.ll;
 
 import com.ll.domain.wiseSaying.controller.WiseSayingController;
-import com.ll.domain.wiseSaying.entity.WiseSaying;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
-    private int lastId;
-    private final List<WiseSaying> wiseSayings;
     private final WiseSayingController wiseSayingController;
 
     public App() {
         scanner = new Scanner(System.in);
-        lastId = 0;
-        wiseSayings = new ArrayList<>();
-        wiseSayingController = new WiseSayingController();
+        wiseSayingController = new WiseSayingController(scanner);
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        makeSampleData();
+        wiseSayingController.makeSampleData();
 
         while (true) {
             System.out.print("명령) ");
@@ -32,46 +25,16 @@ public class App {
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
-                actionAdd();
+                wiseSayingController.actionAdd();
             } else if (cmd.equals("목록")) {
-                wiseSayingController.actionList(wiseSayings);
+                wiseSayingController.actionList();
             } else if (cmd.startsWith("삭제")) {
-                wiseSayingController.actionDelete(wiseSayings, cmd);
+                wiseSayingController.actionDelete(cmd);
             } else if (cmd.startsWith("수정")) {
-                wiseSayingController.actionModify(scanner, wiseSayings, cmd);
-
+                wiseSayingController.actionModify(cmd);
             }
         }
 
         scanner.close();
     }
-
-    private void makeSampleData() {
-        addWiseSaying("나의 죽음을 적들에게 알리지 말라.", "이순신 장군");
-        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
-    }
-
-    private WiseSaying addWiseSaying(String content, String author) {
-        int id = ++lastId;
-
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-
-        wiseSayings.add(wiseSaying);
-
-        return wiseSaying;
-    }
-
-    // 액션 함수들
-    private void actionAdd() {
-        System.out.print("명언 : ");
-        String content = scanner.nextLine();
-        System.out.print("작가 : ");
-        String author = scanner.nextLine();
-
-        WiseSaying wiseSaying = addWiseSaying(content, author);
-
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
-    }
-
-
 }
--- src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
@@ -2,11 +2,48 @@ package com.ll.domain.wiseSaying.controller;
 
 import com.ll.domain.wiseSaying.entity.WiseSaying;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingController {
-    public void actionList(List<WiseSaying> wiseSayings) {
+    private final Scanner scanner;
+    private final List<WiseSaying> wiseSayings;
+    private int lastId;
+
+    public WiseSayingController(Scanner scanner) {//걍 scanner 넣어버림 . 그럼 이제 밑에서 scanner 안써도댐
+        this.scanner = scanner;
+        this.wiseSayings = new ArrayList<>();
+        this.lastId = 0;
+    }
+
+    private WiseSaying addWiseSaying(String content, String author) {
+        int id = ++lastId;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+        wiseSayings.add(wiseSaying);
+
+        return wiseSaying;
+    }
+
+    public void makeSampleData() {
+        addWiseSaying("나의 죽음을 적들에게 알리지 말라.", "이순신 장군");
+        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+    }
+
+    public void actionAdd() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        WiseSaying wiseSaying = addWiseSaying(content, author);
+
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
+    }
+
+    public void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
@@ -15,7 +52,7 @@ public class WiseSayingController {
         }
     }
 
-    public void actionDelete(List<WiseSaying> wiseSayings, String cmd) {
+    public void actionDelete(String cmd) {
         String idStr = cmd.substring(6);
         int id = Integer.parseInt(idStr);
 
@@ -25,7 +62,7 @@ public class WiseSayingController {
         else System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
     }
 
-    public void actionModify(Scanner scanner, List<WiseSaying> wiseSayings, String cmd){
+    public void actionModify(String cmd) {
         String idStr = cmd.substring(6);
         int id = Integer.parseInt(idStr);
 
